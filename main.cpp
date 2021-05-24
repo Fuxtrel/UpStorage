188265614f5769ee45befb7b86dac63371912b96
Alexandr Somov <55055991+Fuxtrel@users.noreply.github.com>
2021-05-24 12:42:58 +0300

    Сделан первый лист приложения, без картинок

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("/home/alex/Pictures/1.png"));
     MainWindow w;
     w.show();
     return a.exec();
